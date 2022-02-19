bece9297d5305d60533ddf392a11edaf1f8aad11
TheSilvaDev <66764107+thesilvaofficial@users.noreply.github.com>
2022-02-19 12:19:09 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 TheSilva = {}
 
-TheSilva.everyoneAllowed = false -- true = everyone is allowed || false = use steamIDs and IPs listed in allowed
+TheSilva.EveryoneAllowed = false -- true = everyone is allowed || false = use steamIDs and IPs listed in allowed
 
 TheSilva.AllowedList = { -- Enter your steam ids and ips as such. DO NOT forget the commas and do not add a comma at the end.
 	"steam:000000000000000", 
