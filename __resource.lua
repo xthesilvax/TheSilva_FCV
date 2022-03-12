ebe3fe171b18970b6268a1cd3af0c951ff887f3b
TheSilvaDev <66764107+xthesilvax@users.noreply.github.com>
2022-03-12 16:56:23 +0000

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,17 +1,1 @@
-fx_version 'adamant'
-game 'gta5'
-
-server_scripts { 
-	'server/main.lua', 
-	'config.lua' 
-}
-
-client_scripts { 
-	'client/main.lua', 
-	'config.lua' 
-}
-
-name 'TheSilva_FCV'
-version '1.1'
-description 'Fix and Clean Command.'
-author 'TheSilvaDev'
+1
